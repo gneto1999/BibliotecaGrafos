87921ddc44e4010b3eef003ab6bdf8f4cda67039
Gilmar Santos <gneto1999@hotmail.com>
2023-06-05 17:36:09 -0300

    Inicialização do projeto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String path = "src/grafo_1.txt";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+
+            while(line != null) {
+                System.out.print(line+"\n");
+                line = br.readLine();
+            }
+        } catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
